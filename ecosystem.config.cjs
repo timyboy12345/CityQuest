59335fd1d0e673a011fb54cf51b3fc3e3185767f
Tim Arendsen <t.j.m.arendsen@gmail.com>
2026-01-30 13:35:07 +0100

    Updated ecosystem.js v2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'cityquest-app',
-            port: '3014',
+            port: '5173',
             exec_mode: 'cluster',
             instances: 'max',
             script: './.output/server/index.mjs',
