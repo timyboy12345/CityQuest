c71e4ad821eee1fc891ee37c854275a19e6440d8
Tim Arendsen <t.j.m.arendsen@gmail.com>
2026-02-06 14:37:33 +0100

    v1.0.7: Improved map, added deploy script

--- app/components/quest-components/MapComponent.vue
+++ app/components/quest-components/MapComponent.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
-const {step} = defineProps(['zoom', 'center', 'ownLocation', 'quest', 'step', 'targetLocation', 'targetRadius'])
+const {step} = defineProps<{
+  zoom: number,
+  center: [],
+  ownLocation: [],
+  quest: unknown,
+  step: unknown,
+  targetLocation: [],
+  targetRadius: number,
+}>()
 defineEmits(['mapClick'])
 
 const poly = computed(() => step.item.polygon.coordinates[0].map((l) => [l[1], l[0]]));
@@ -10,27 +18,27 @@ const poly = computed(() => step.item.polygon.coordinates[0].map((l) => [l[1], l
       ref="map"
       :center="center"
       :options="{zoomControl: false, attributionControl: false}"
-      :useGlobalLeaflet="false"
+      :use-global-leaflet="false"
       :zoom="zoom"
-      class="absolute -z-0"
+      class="left-0 top-0 absolute -z-0"
       @click="$emit('mapClick', $event.latlng)"
   >
     <LTileLayer
         url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         layer-type="base"
         name="StoryWalks Map"
-    ></LTileLayer>
+    />
 
-    <l-circle-marker v-if="ownLocation" :lat-lng="ownLocation"></l-circle-marker>
+    <l-circle-marker v-if="ownLocation" :lat-lng="ownLocation"/>
     <l-circle-marker
         v-if="step.item.polygon.type === 'Circle'"
         :lat-lng="targetLocation"
         :radius="targetRadius"
-        color="#fff"></l-circle-marker>
+        color="#fff"/>
     <l-polygon
         v-else-if="step.item.polygon.type === 'Polygon'"
         :lat-lngs="poly"
-    ></l-polygon>
+    />
   </LMap>
 </template>
 
